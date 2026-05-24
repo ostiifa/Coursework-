f4151f800d8e08c0b0275b6a50d778be8017e319
Ksemor <ksemorjinjer@gmail.com>
2026-05-24 14:34:51 +0300

    написал файл с реализациями функций Account.cpp

---
+++ Account.cpp
@@ -0,0 +1,70 @@
+﻿#include "Account.h"
+#include <iostream>
+#include <cstring>
+
+namespace PaymentCore {
+	Account createAccount(int id, const char* name, double initialBalance) {//описание функции создания аккаунта
+		Account newAcc;
+		newAcc.id = id;
+		newAcc.balance = balance;
+		newAcc.isActive = true;
+
+		#ifdef _MSC_VER 
+			strcpy_s(newAcc.ownerName, MAX_NAME_LEN, name);
+		#else
+			std:strpy_s(newAcc.ownerName, name, MAX_NAME_LEN - 1);
+			newAcc.ownerName[MAX_NAME_LEN - 1] = '\0';
+		#endif
+
+		return newAcc;
+	}
+
+	void printAccount(const Account& acc) {//печать аккаунта
+		if (!acc.isActive) return;
+		std::cout << "[Счет #" << acc.id << "] "
+			<< "[Владелец: " << acc.ownerName << " | "
+			<< "Баланс: " << acc.balance << " BYN" << std::endl;
+	}
+
+	void addAccountNode(AccountNode*& head, const Account& acc) {//добавление узла в кучу
+		AccountNode* newNode = new AccountNode;
+		newNode->data = acc;
+		newNode->next = nullptr;
+
+		if (head == nullptr) {
+			head = newNode;
+			return;
+		}
+
+		AccountNode* current = head;
+		while (current->next != nullptr) {
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+
+	AccountNode* findAccountById(AccountNote* head, int id) {//поиск по id
+		AccountNode* current = head;
+		while (current != nullptr) {
+			if (current->data.id == id && current->data.isActive) {
+				return current;
+			}
+			current = current->next;
+		}
+		return nullptr;
+	}
+
+	void clearList(AccountNode*& head) {//очистка ОЗУ от списка
+		AccountNode* current = head;
+		int count = 0;
+		while (current != nullptr) {
+			AccountNode* nextNode = current->next;
+			delete current;
+			current = nextNode;
+			count++
+		}
+		head = nullptr;
+		std::cout << "[System Log] Очищено узлов в памяти: " << count << std::endl;
+	}
+
+}
