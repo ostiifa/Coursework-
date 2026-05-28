e8f0a81810105c64870221bc2ed071664e0264eb
Ksemor <ksemorjinjer@gmail.com>
2026-05-28 22:35:21 +0300

    изменение common.h

--- Common.h
+++ Common.h
@@ -1,11 +1,11 @@
-#ifndef COMMON_H
+﻿#ifndef COMMON_H
 #define COMMON_H
 
 namespace PaymentCore {
-    // Константы Программиста А (исправили пропущенную точку с запятой)
+    // Константы Программиста А
     const int MAX_NAME_LEN = 50; // определение длины имени аккаунта
 
-    // Константы Программиста Б (Твои константы для бинарного хранилища)
+    // Константы Программиста Б
     const int DB_VERSION = 1;
     const int BLOCK_SIZE = 256;
 }
